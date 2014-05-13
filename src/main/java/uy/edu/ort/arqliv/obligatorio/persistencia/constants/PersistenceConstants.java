c3157dbe24f678f01a1c29c4de728061e2b0de3e
Rodrigo <rodrigodemi@gmail.com>
2014-05-12 23:01:32 -0300

    agrego interface con las constantes de los nombres de los beans

---
+++ src/main/java/uy/edu/ort/arqliv/obligatorio/persistencia/constants/PersistenceConstants.java
@@ -0,0 +1,9 @@
+package uy.edu.ort.arqliv.obligatorio.persistencia.constants;
+
+public interface PersistenceConstants {
+
+	public String ShipDao = "shipDAO";
+	public String ContainerDao = "containerDAO";
+	public String ArrivalDao = "arrivalDAO";
+	
+}
